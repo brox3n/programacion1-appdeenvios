9ec369411b8e0ee30423f7c96557052e165e42d2
brox3n <145502795+brox3n@users.noreply.github.com>
2024-08-29 17:22:37 -0300

    Update core.py

--- modulo_funciones /core.py
+++ modulo_funciones /core.py
@@ -1,15 +1,4 @@
 #Función que será utilizada para arrancar el programa
 
-'' import os
 
-def menu ()
-  repetir = True
-  while repetir:
-    os.system ("cls") 
-    print ("1- ...")
-    print . . .
-    .
-    .
-    .
-    ''
     
